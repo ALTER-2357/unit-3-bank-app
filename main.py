d7d23d49466ad35d059f6eaa31ab2a24073ce4a8
justjakie <80701672+justjakie@users.noreply.github.com>
2021-11-12 14:21:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+p = 0
+b = 0
+s = 0
+user_id = "69420"
+password = (p)
+account_holder = 'James'
+balance = (b)
+sortcode = (s)
+account_number = 4206921
 
-"""Top-level script to invoke helloworld implementation."""
-
-import sys
-import helloworld.main
-
-if __name__ == '__main__':
-    sys.exit(helloworld.main.main()).
+print ("Welcome " + user_id)
